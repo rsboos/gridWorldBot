aa5076964e6d186f40642722ebab760a7d23c650
Rodrigo Boos <rsboos@github.com>
2015-11-11 02:49:12 -0200

    finishing first version

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@
 using namespace std;
 
 
-#define GAMMA 0.9 // taxa de desconto
-#define ALPHA 0.1
+#define GAMMA 0.8 // taxa de desconto
+#define ALPHA 0.2
 #define UP 0
 #define DOWN 1
 #define LEFT 2
 #define RIGHT 3
-#define INF 9999999999
+#define INF 999999
 
 std::vector< std::vector<float> > q;
 std::vector< std::vector<float> > r;
@@ -89,13 +89,14 @@ int getNeighbour(int pos, int action) {
 				return "RIGHT";
 				break;
 			default:
-				return NULL;
+				return "N";
 				break;
 		}	
 	}
 	float calculateQ(int pos, int action) {
 		float ret = 0;
-		ret = (1-ALPHA) * r[pos][action];
+		//ret = q[pos][action];
+		ret = (1-ALPHA) * q[pos][action];
 		int nextState = getNeighbour(pos,action);
 		float maxFactor = -INF;
 		float val;
@@ -105,7 +106,7 @@ int getNeighbour(int pos, int action) {
 			if (val > maxFactor)
 				maxFactor = val;
 		}
-		ret += ALPHA*GAMMA*maxFactor;
+		ret += ALPHA * (r[pos][action] + (GAMMA*maxFactor));
 		return ret;
 	}
 
@@ -127,12 +128,23 @@ int getNeighbour(int pos, int action) {
 
 	bool hasEquals(vector<float> &v,float bestValue, int pos, vector<int> &ret) {
 		int totEqual = 0;
+		vector<int> retSub;
 		for (int i=0; i < v.size(); i++)
 			if (v[i] == bestValue) {
 				totEqual++;
-				if (!(getNeighbour(pos,i) == pos)) 
-					ret.push_back(i);
+				if (!(getNeighbour(pos,i) == pos)) { // not going to the same place
+					retSub.push_back(i);
+					if (!visited[getNeighbour(pos,i)]) // better if not visited
+						ret.push_back(i);
+				}
 			}
+		if (ret.size() == 0) {
+			ret.clear();
+			for (int i=0; i < retSub.size(); i++) {
+				ret.push_back(retSub[i]);
+			}
+		}
+
 
 		if (totEqual > 1)
 			return true;
@@ -141,73 +153,116 @@ int getNeighbour(int pos, int action) {
 	}
 
 	bool performRandomAction() {
-		int probConst = 80;
+		int probConst = 50;
 		int eta = count(visited.begin(),visited.end(),true) / totalValidCells;
 		eta = eta / 2; // amortize constant
-		int probRes = probConst - eta;
+		int probRes = probConst;
 		srand (time(NULL));
 		int randNum = rand() % 100;
-		if (randNum < probRes)
+		if (randNum < probConst)
 			return true;
 		else
 			return false;
 	}
 
-	void performEpisode(int pos) {
+	void selectActions(int pos, vector<int> &v) {
+		vector<int> retSub;
+		for (int i=0; i < 4; i++)
+			if (!(getNeighbour(pos,i) == pos)) { // not going to the same place
+				retSub.push_back(i);
+					if (!visited[getNeighbour(pos,i)]) // better if not visited
+						v.push_back(i);
+			}
+		if (v.size() == 0) {
+			v.clear();
+			for (int i=0; i < retSub.size(); i++) {
+				v.push_back(retSub[i]);
+			}
+		}
+	}
+
+	int performEpisode(int pos) {
+		for (int i=0; i < nLines*nColumns; i++)
+			visited[i]= false;
+
+		int totalSteps = 0;
 		bool isFinalState = false;
-		vector<float> valuesTmp;
-		vector<float> values;
-		values.resize(4);
-		valuesTmp.resize(4);
+		vector<float> valuesTmp; // 
+		vector<float> values; // 
+		values.resize(4); // 
+		valuesTmp.resize(4); 
 		int curPos = pos;
-		do {
-			srand (time(NULL));
+		do {			
 			visited[curPos] == true;
 			int bestMove;
-			float bestValue;
+			float bestValue; 
 			if (performRandomAction()) {
-				bestMove = rand() % 4;
+				vector<int> actions;
+				selectActions(curPos,actions);
+				if (actions.size() == 0) {
+					bestMove = rand() % 4;
+				}
+				else {
+					int bestMoveIndex = rand() % actions.size();
+					bestMove = actions[bestMoveIndex];
+				}
 				bestValue = calculateQ(curPos,bestMove);
-				vector<float> v;
+				vector<float> v; // float
 				v.push_back(bestValue);
 				bestValue = normalizeQ(0,v);
+				if (q[curPos][bestMove] < bestValue)
+					q[curPos][bestMove] = bestValue;
 			}
-			else {			
+			else {
+				srand (time(NULL));			
 				for (int i=0; i < valuesTmp.size(); i++)
-					valuesTmp[i] = calculateQ(curPos,i);	
+					valuesTmp[i] = calculateQ(curPos,i); 	
 				for (int i=0; i < values.size(); i++) {
-					values[i] = normalizeQ(i,valuesTmp);
+					values[i] = normalizeQ(i,valuesTmp); 
 					//cout << values[i] << endl;
 				}
 				srand (time(NULL));
 				bestValue = *max_element(values.begin(),values.end());
 				
 				vector<int> bestPositions;
-				if (hasEquals(values,bestValue,curPos,bestPositions)) {
+				if (hasEquals(values,bestValue,curPos,bestPositions)) { // float
+					if (bestPositions.size() == 0) {
+						size_t tmp = max_element(values.begin(),values.end()) - values.begin();
+						bestMove = static_cast<int>(tmp);
+					}
+					else {
 					int randVal = rand() % bestPositions.size();
 					bestMove = bestPositions[randVal];
+					}
 				}
 				else {
 					size_t tmp = max_element(values.begin(),values.end()) - values.begin();
 					bestMove = static_cast<int>(tmp);
 				}
+				q[curPos][bestMove] = bestValue;
+			}
+			if (curPos == 10) {
+				//cout << "10 will execute: " << actionPrint(bestMove) << endl;
 			}
 
-			q[pos][bestMove] = bestValue;
+			
 		//int bestMove = find(values.begin(),values.end(),bestValue) - values.begin(); // UP, DOWN, LEFT, RIGHT
 		//cout << bestMove << endl;
 		//cout << bestValue << endl;
 		
 		int nextPos = getNeighbour(curPos,bestMove);
 		if (curPos != nextPos) {
-			cout << "Current position: " << curPos << " Next one: " << nextPos << endl;
+			//cout << "Current position: " << curPos << " Next one: " << nextPos << endl;
 		}
 		//cout << "Current position: " << curPos << " Next one: " << nextPos << " Move made: " << actionPrint(bestMove) << endl;
 		if (isFinal[nextPos])
 			isFinalState=true;
 		curPos = nextPos;
-	
+		totalSteps++;
 	} while (!isFinalState);
+	cout << "Perform made - > Current position: " << curPos;
+	return totalSteps;
+	
 
 }
 
@@ -227,6 +282,31 @@ void initVars() {
 			r[i][RIGHT] = (0.8*reward[rightNode]) + (0.1 * reward[upNode]) + (0.1 * reward[downNode]);
 		}
 	}
+}
+
+void printQ() {
+	for (int i=0; i < nLines; i ++) {
+		for (int j=0; j < nColumns; j++) {
+			int pos = getIndex(i,j);
+			int bestMove = -1;
+			if (!isWall(pos)) {
+				float bestVal = -INF;
+				for (int index = 0; index < 4; index++) {
+					if ((q[pos][index] != 0) && (q[pos][index] > bestVal)) {
+						bestVal = q[pos][index];
+						bestMove = index;
+					}
+				}
+				cout << actionPrint(bestMove) << "\t";	
+			}
+			else
+				cout << "X\t";
+						
+		}
+		cout << endl;
+	}
+	int i=0;
+	cout << q[i][0] << " " << q[i][1] << " " << q[i][2] << " " << q[i][3] << endl;
 }
 
 int main() {
@@ -251,7 +331,7 @@ int main() {
 		if (content == "X") {
 			//cout << q[i].size();
 		}
-		else if (content == "D") {
+		else if ((content == "D") || (content == ".")) {
 			totalValidCells++;
 			reward[i] = defaultValue;
 			vector< float > qElement, rElement;
@@ -284,7 +364,15 @@ int main() {
 		initVars();
 	// end of initing vars
 	int initState = 0;
-	performEpisode(initState);
+	int bestFound = INF;
+	for (int i=0; i < 200; i++) {
+		int totSteps = performEpisode(initState);
+		if (totSteps < bestFound)
+			bestFound = totSteps;
+		cout << "Episode " << i+1 << " finished with " << totSteps << " steps." << endl;
+		printQ();
+	}
+	cout << "best: " << bestFound << endl;
 
 	return 0;
 }
